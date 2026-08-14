a6347169160238441463983f57aceb24fff21502
Yevhen <evgenzuzin35@gmail.com>
2026-08-14 20:44:52 +0300

    Add solution for task1

---
+++ assets/js/task1.js
@@ -0,0 +1,18 @@
+const fileNames = [
+  "index.html",
+  "styles.css",
+  "script.js",
+  "about.html",
+  "contact.html",
+  "home.html",
+  "blog-post-1.html",
+  "blog-post-2.html",
+  "services.html",
+  "portfolio.html",
+  "projects.html",
+  "gallery.html",
+  "faq.html",
+  "terms.html",
+  "privacy-policy.html",
+];
+const filteredNames = fileNames.filter(elem=>elem.endsWith('.html'));
